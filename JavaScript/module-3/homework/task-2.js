1334d3e6054b7e0b0faa55cbade53b40094c9a70
Mac <mac@Yura-Yakovenkos-iMac.local>
2020-05-05 20:10:42 +0300

    homework 3 task 3 complete

--- JavaScript/module-3/homework/task-2.js
+++ JavaScript/module-3/homework/task-2.js
@@ -1,1 +1,16 @@
 console.log('~2~');
+
+const countProps = function(obj) {
+  const keys = Object.keys(obj);
+  let props = 0;
+  for (let i = 0; i < keys.length; i += 1) {
+    props += 1;
+  }
+  return `Кол-во свойств в обьекте: ${props}`;
+};
+
+console.log(countProps({})); // 0
+
+console.log(countProps({ name: 'Mango', age: 2 })); // 2
+
+console.log(countProps({ mail: 'poly@mail.com', isOnline: true, score: 500 })); // 3
--- JavaScript/module-3/homework/task-3.js
+++ JavaScript/module-3/homework/task-3.js
@@ -1,20 +1,41 @@
 console.log('~3~');
 
-const findLongestWord = function(string) {
-  const arrayOfStrings = string.split(' ');
-  let longestWord = 0;
-  let maxlengthWord = null;
-  for (let i = 0; i < arrayOfStrings.length; i += 1) {
-    if (arrayOfStrings[i].length > longestWord) {
-      longestWord = arrayOfStrings[i].length;
-      maxlengthWord = arrayOfStrings[i];
+const findBestEmployee = function(employees) {
+  const employeeName = Object.keys(employees);
+  const employeeProd = Object.values(employees);
+  const maxProd = Math.max(...employeeProd);
+  for (const key of employeeName) {
+    if (maxProd === employees[key]) {
+      return `Самый продуктивный работник ${key}: ${maxProd}`;
     }
   }
-  return maxlengthWord;
 };
 
-console.log(findLongestWord('The quick brown fox jumped over the lazy dog')); // 'jumped'
+/*
+ * Вызовы функции для проверки работоспособности твоей реализации.
+ */
+console.log(
+  findBestEmployee({
+    ann: 29,
+    david: 35,
+    helen: 1,
+    lorence: 99,
+  }),
+); // lorence
 
-console.log(findLongestWord('Google do a roll')); // 'Google'
+console.log(
+  findBestEmployee({
+    poly: 12,
+    mango: 17,
+    ajax: 4,
+  }),
+); // mango
 
-console.log(findLongestWord('May the force be with you'));
+console.log(
+  findBestEmployee({
+    lux: 147,
+    david: 21,
+    kiwi: 19,
+    chelsy: 38,
+  }),
+); // lux
