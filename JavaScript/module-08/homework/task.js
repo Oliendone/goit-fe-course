d30c72a38f1d442242fe628c5f2dd797c3e5e0d7
Nikita <Zungarcheg@gmail.com>
2020-07-22 22:11:42 +0300

    homework 8 arrows control finish

--- JavaScript/module-08/homework/task.js
+++ JavaScript/module-08/homework/task.js
@@ -25,6 +25,7 @@ const addPictures = images => {
     src="${image.preview}"
     data-source="${image.original}"
     alt="${image.description}"
+    data-index="${image.index}"
   />
 </a>
 </li>`,
@@ -37,15 +38,40 @@ const addPictures = images => {
 addPictures(gallery);
 
 // =============functions================
-const closeModalEsc = () => {
+let imageIndex;
+
+const functionalKeys = event => {
+  let arrayLength = gallery.length;
+  let arrayElement;
   if (event.code === 'Escape') {
     closeModal();
+  }
+  if (event.code === 'ArrowRight') {
+    // if (arrayElement === arrayLength) {
+    //   arrayElement = gallery.find(image => Number(image.index) === 0);
+    //   console.log(arrayElement);
+    //   imageRef.setAttribute('src', arrayElement.original);
+    // }
+    arrayElement = gallery.find(
+      image => Number(image.index) === imageIndex + 1,
+    );
+    imageRef.setAttribute('src', arrayElement.original);
+    imageIndex += 1;
+    console.log(arrayLength);
+  }
+  if (event.code === 'ArrowLeft') {
+    arrayElement = gallery.find(
+      image => Number(image.index) === imageIndex - 1,
+    );
+    imageRef.setAttribute('src', arrayElement.original);
+    imageIndex -= 1;
   }
 };
 
 const openModal = event => {
-  window.addEventListener('keydown', closeModalEsc);
+  window.addEventListener('keydown', functionalKeys);
   const imageUrl = event.target.dataset.source;
+  imageIndex = Number(event.target.dataset.index);
   event.preventDefault();
   if (event.target.nodeName !== 'IMG') {
     return;
@@ -55,7 +81,7 @@ const openModal = event => {
 };
 
 const closeModal = () => {
-  window.removeEventListener('keydown', closeModalEsc);
+  window.removeEventListener('keydown', functionalKeys);
   lightboxRef.classList.remove('is-open');
   imageRef.setAttribute('src', '');
 };
